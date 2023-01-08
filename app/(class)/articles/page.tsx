5ad8a8a2954b0f7f2ab13590ecb498a8e20596e6
Wenson <wensonsmith@gmail.com>
2023-01-08 17:21:09 +0800

    feat: articles

--- app/(class)/articles/page.tsx
+++ app/(class)/articles/page.tsx
@@ -1,7 +1,27 @@
-export default function Articles() {
+import ArticleCard from "../../../components/article-card"
+import { fetchArticles } from "../../../lib/strapi"
+
+export default async function Articles() {
+  const response = await fetchArticles({
+    filters: {},
+    populate: {
+      tags: "*",
+    },
+  })
+
+  const articles = response.data
+
   return (
-    <>
-      <h3>Articles List</h3>
-    </>
+    <div className="container m-auto pt-10">
+      <div className="text-xl font-light mb-6 font-mono">
+        ARTICLES <span className="font-xl text-red-500 font-medium">/</span> 全部文章
+      </div>
+
+      <div className="grid md:grid-cols-4 grid-cols-1 gap-5">
+        {articles.map((article: any) => (
+          <ArticleCard article={article} key={article.id} />
+        ))}
+      </div>
+    </div>
   )
 }
