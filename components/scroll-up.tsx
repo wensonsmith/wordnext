1adc1c1c2cc6af636bcb3783efd30aa65a1ce048
Wenson <wensonsmith@gmail.com>
2026-08-27 11:22:39 +0800

    fix: prevent invalid effect cleanup during navigation

--- components/scroll-up.tsx
+++ components/scroll-up.tsx
@@ -6,7 +6,9 @@ import { usePathname } from "next/navigation"
 export default function ScrollUp() {
   const pathname = usePathname()
 
-  useEffect(() => window.document.scrollingElement?.scroll(0, 0), [pathname])
+  useEffect(() => {
+    window.document.scrollingElement?.scrollTo(0, 0)
+  }, [pathname])
 
   return null
 }
